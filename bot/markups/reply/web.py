365d05535ea68d6bc73d460db5b0e6d6e350bf3b
caVenikk <sqqvt1@gmail.com>
2023-01-16 13:50:09 +0300

    Added web app keyboard

---
+++ bot/markups/reply/web.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, WebAppInfo, KeyboardButton
+
+from bot.loader import config
+
+web_app = WebAppInfo(url=f"{config.web_info.webapp_url}/")
+
+web_app_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Site", web_app=web_app)]
+    ],
+    resize_keyboard=True
+)

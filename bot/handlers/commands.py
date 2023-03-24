7b8c0e809d5dbf17542d0a0645437884d1614481
caVenikk <sqqvt1@gmail.com>
2023-03-24 19:15:25 +0300

    User register to db

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,13 +1,25 @@
+import aiohttp
 from aiogram.types import Message
 
-from bot.loader import dp, bot
+from bot.loader import dp, bot, accessor
 from bot.markups.reply import web_app_keyboard
+from utils.schemas.users import User
 
 
-@dp.message_handler(commands=['start', 'order'], state='*')
+@dp.message_handler(commands=["start", "order"], state="*")
 async def start_handler(message: Message):
+    user = User(
+        id=message.from_user.id,
+        first_name=message.from_user.first_name,
+        second_name=message.from_user.last_name,
+        username=message.from_user.username,
+    )
+    async with aiohttp.ClientSession() as client:
+        await accessor.add_user(client, user)
+
+    text = "Приступим к заказу!🍟\nНажмите на кнопку ниже или в меню, чтобы заказать идеальный обед!🌯"
     await bot.send_message(
         chat_id=message.chat.id,
-        text='Оформите заказ, нажав на кнопку ниже!',
-        reply_markup=web_app_keyboard
+        text=text,
+        reply_markup=web_app_keyboard,
     )
--- utils/accessor/accessor.py
+++ utils/accessor/accessor.py
@@ -1,5 +1,6 @@
 from utils import Config
 from utils.schemas import Product, Order
+from utils.schemas.users import User
 
 
 class Accessor:
@@ -13,10 +14,36 @@ class Accessor:
             products = [Product(**product) for product in result_json]
             return products
 
-    async def add_order(self, client, order: Order):
+    async def get_user(self, client, user_id):
+        async with client.get(f"{self._config.web_info.api_url}/users/{user_id}") as resp:
+            assert resp.status == 200
+            user = await resp.json()
+            return User(**user)
+
+    async def update_user(self, client, user):
+        async with client.put(
+                f"{self._config.web_info.api_url}/users", json=user.to_dict()
+        ) as resp:
+            assert resp.status == 200
+            user = await resp.json()
+            return user
+
+    async def add_user(self, client, user):
+        db_user = await self.get_user(client, user.id)
+        if db_user:
+            await self.update_user(client, user)
+            return db_user
         async with client.post(
-            f"{self._config.web_info.api_url}/orders", json=order.to_dict()
+                f"{self._config.web_info.api_url}/users", json=user.to_dict()
         ) as resp:
             assert resp.status == 201
-            result_json = await resp.json()
-            return result_json
+            user = await resp.json()
+            return user
+
+    async def add_order(self, client, order: Order):
+        async with client.post(
+                f"{self._config.web_info.api_url}/orders", json=order.to_dict()
+        ) as resp:
+            assert resp.status == 201
+            order = await resp.json()
+            return order

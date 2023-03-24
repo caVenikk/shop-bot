efc467a1535c95b2fce0ff0ee179a71f292a69c1
caVenikk <sqqvt1@gmail.com>
2023-03-24 18:46:00 +0300

    Making invoice link

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,15 @@
+import asyncio
+
+import nest_asyncio
 from aiogram.utils.executor import start_polling, Dispatcher
+from aiohttp import web
 from loguru import logger
 
 from bot.handlers import dp
+from bot.loader import app
+from bot.views.invoice import InvoiceLinkView
+
+nest_asyncio.apply()
 
 
 def on_events():
@@ -17,7 +25,14 @@ def on_events():
     return startup, shutdown
 
 
-if __name__ == '__main__':
+async def main():
+    app.router.add_view('/create_invoice_link', InvoiceLinkView)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '127.0.0.1', 8888)
+    await site.start()
+    logger.info(f"Web app is running on {site.name}")
+
     logger.info(f"Number of message handlers: {len(dp.message_handlers.handlers)}.")
     logger.info(f"Number of callback query handlers: {len(dp.callback_query_handlers.handlers)}.")
     on_startup, on_shutdown = on_events()
@@ -27,3 +42,7 @@ if __name__ == '__main__':
         on_startup=on_startup,
         on_shutdown=on_shutdown,
     )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- bot/loader.py
+++ bot/loader.py
@@ -1,5 +1,7 @@
+import aiohttp_cors
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiohttp import web
 
 from utils import Config
 from utils.accessor import Accessor
@@ -7,5 +9,18 @@ from utils.accessor import Accessor
 config = Config.load()
 accessor = Accessor()
 memory_storage = MemoryStorage()
-bot = Bot(token=config.telegram.bot_token, parse_mode='HTML')
+bot = Bot(token=config.telegram.bot_token, parse_mode="HTML")
 dp = Dispatcher(bot, storage=memory_storage)
+app = web.Application()
+
+cors = aiohttp_cors.setup(
+    app,
+    defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True, expose_headers="*", allow_headers="*"
+        )
+    },
+)
+
+for route in list(app.router.routes()):
+    cors.add(route)

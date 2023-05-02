1b5a99726ce6e608b71f0e552f42b62fa99abec5
caVenikk <sqqvt1@gmail.com>
2023-05-02 14:19:35 +0300

    Refactor for multiple products

--- bot/views/invoice.py
+++ bot/views/invoice.py
@@ -18,40 +18,43 @@ class InvoiceLinkView(View):
                 reason="Bad request",
             )
         try:
-            request_product = Product.from_dict(data["product"])
+            request_products = [Product.from_dict(product) for product in data["products"]]
             user_id = data["user_id"]
         except KeyError:
             return json_response(
                 status=402,
                 reason="Unprocessable Entity",
             )
-        if not request_product:
+        if not request_products:
             return json_response(
                 status=400,
                 reason="Bad request",
             )
         async with aiohttp.ClientSession() as client:
             db_products = await accessor.get_products(client)
+            last_order_id = await accessor.get_last_order_id(client)
         if db_products is None:
             return json_response(
                 status=500,
                 reason="Internal Server Error",
             )
-        product = next(
-            (product for product in db_products if product == request_product), None
-        )
-        if not product:
-            return json_response(
-                status=404,
-                reason="Product not found",
-            )
-        await dp.storage.set_data(user=user_id, data=product)
+        products = []
+        for req_product in request_products:
+            product = next((db_product for db_product in db_products if db_product == req_product), None)
+            if not product:
+                return json_response(
+                    status=404,
+                    reason="Product not found",
+                )
+            products.append(product)
+        await dp.storage.set_data(user=user_id, data=products)
         invoice_link = await bot.create_invoice_link(
-            title=product.title,
-            description=product.description if product.description else product.title,
+            title=f"Заказ #{last_order_id}",
+            description="Идеальный обед с McBonald's",
+            photo_url="https://hips.hearstapps.com/hmg-prod/images/190130-chicken-shwarma-horizontal-1549421250.png?crop=1xw:0.843328335832084xh;center,top",
             provider_token=config.telegram.payment_token,
             currency="rub",
-            prices=product.price,
+            prices=[product.price for product in products],
             need_name=True,
             need_phone_number=True,
             need_shipping_address=True,
